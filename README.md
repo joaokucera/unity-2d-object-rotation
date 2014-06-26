0f5f19c79a7123cbd58325a9cdb86008f3399e14
João Ricardo Kucera <falleirok@gmail.com>
2014-06-26 14:41:24 -0300

    Complete sample.
    Complete sample.

---
+++ README.md
@@ -0,0 +1,4 @@
+A object rotation script to unity 3d
+====================================
+
+The proposal here was to create a object rotation script based on touch to be used in 2d unity projects.
---
+++ source/Assets/Scripts/Rotation.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+    /// <summary>
+    /// Speed to rotate.
+    /// </summary>
+    [SerializeField] private float turnSpeed = 5;
+
+    /// <summary>
+    /// Movement.
+    /// </summary>
+    private Vector2 movement;
+
+    void Update()
+    {
+        Vector2 currentPosition = transform.position;
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 moveTowards = Camera.main.ScreenToWorldPoint(touch.position);
+
+                movement = moveTowards - currentPosition;
+                movement.Normalize();
+            }
+        }
+
+        float targetAngle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Slerp(transform.rotation, 
+                                              Quaternion.Euler(0, 0, targetAngle), 
+                                              turnSpeed * Time.deltaTime);
+    }
+}
